d40dac28bc31cc543b4d3c394f92e96807935cd4
nandish <79184940+nandish1199@users.noreply.github.com>
2021-04-14 11:27:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+ // ##################burger#################
+ let burger = document.querySelector('#burger');
+ let navcross = document.querySelector('#nav-cross');
+ function navshow(){
+   document.querySelector('.nav-links').style.display="block";
+ };
+ 
+ function navhide(){
+   document.querySelector('.nav-links').style.display="none";
+ };
